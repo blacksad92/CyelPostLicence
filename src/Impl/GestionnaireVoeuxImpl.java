e64df97229bc8efd363b254b150c8e713e1b0489
RomainVabre <romainvabrepro@gmail.com>
2015-06-19 16:25:23 +0200

    Suppresion balise merge

--- src/Impl/GestionnaireVoeuxImpl.java
+++ src/Impl/GestionnaireVoeuxImpl.java
@@ -138,19 +138,7 @@ public class GestionnaireVoeuxImpl extends CyelPostLicence.GestionnaireVoeuxPOA 
         //System.out.println(liste.size());
         for (int i = 0; i < liste.size(); i++) {
             GestionnaireCandidatures gestCand = gestAcces.obtenirGestionnaireCandidatures(liste.get(i).numUniv);
-<<<<<<< HEAD
-            if (gestCand != null) {
-                gestCand.RAZPeriode();
-            }
-        }
 
-        try {
-            bdd.bdd_RAZ(academie.numAcademie);
-            System.out.println("La période a été remis à : " + periode);
-        } catch (SQLException ex) {
-            Logger.getLogger(GestionnaireVoeuxImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-=======
             if (gestCand != null){
                 gestCand.RAZPeriode();
                 bdd.bdd_RAZ(academie.numAcademie);
@@ -161,7 +149,7 @@ public class GestionnaireVoeuxImpl extends CyelPostLicence.GestionnaireVoeuxPOA 
          } catch (SQLException ex) {
          Logger.getLogger(GestionnaireVoeuxImpl.class.getName()).log(Level.SEVERE, null, ex);
          }
->>>>>>> bdb3f4d2dfa4c8dba726a28cb44c66c884f34da0
+
 
     }
 
